baa7f33716577652bbf920f49970e61b3f2725b3
hhheyahe <146327240+hhheyahe@users.noreply.github.com>
2024-03-18 20:33:21 +0300

    Update lab3task7.js

--- lab3task7.js
+++ lab3task7.js
@@ -1,1 +1,7 @@
-console.log("Hello!");
+const express = require('express');
+const app = express();
+const port = 80;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
